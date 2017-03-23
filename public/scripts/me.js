34c978f6f7b4fa17e26aa668bccb730e8495fdfa
Steve Larsen <stevelarsen01@gmail.com>
2017-03-23 13:23:56 -0500

    home page content

---
+++ public/scripts/me.js
@@ -0,0 +1,25 @@
+const steveLarsen = {
+    missionDriven: true,
+    abilityToDesign: true,
+    cravesResults: true,
+    teamPlayer: true,
+    fundimentalBeliefInAgile: true,
+    stengthfinders: [
+        'Futuristic',
+        'Strategic',
+        'Ideation',
+        'Connectedness',
+        'Intellection'
+    ],
+    wickedSenseOfHumor: true,
+    favoriteQuotes: [{
+            author: 'Albert Einstein',
+            quote: 'Everything should be made as simple as possible,' +
+                ' and no simpler'
+        },
+        {
+            author: 'Branch Rickey',
+            quote: 'Luck is the residue of good design'
+        }
+    ]
+};
